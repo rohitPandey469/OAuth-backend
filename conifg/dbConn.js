a40161fd9b7cea1646d41c68198886b125c9c4fb
rohitPandey469 <rd8614196@gmail.com>
2023-11-30 14:33:22 +0530

    Added mongo link

--- conifg/dbConn.js
+++ conifg/dbConn.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
 const DB_URL =
-  process.env.DATABASE_URI || "mongodb://127.0.0.1:27017/dizania-example";
+  process.env.DATABASE_URI 
+  // || "mongodb://127.0.0.1:27017/dizania-example";
 const dbConn = () => {
   mongoose.connect(DB_URL, {
     useNewUrlParser: true,
